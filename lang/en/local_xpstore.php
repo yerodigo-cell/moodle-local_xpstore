7f638c78a52fa20edf90ba256b64dad32479ed8c
yerodigo-cell <yerodigo@gmail.com>
2026-05-07 01:45:02 -0500

    Update local_xpstore.php

--- lang/en/local_xpstore.php
+++ lang/en/local_xpstore.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * XP Store (local_xpstore)
+ *
+ * @package     local_xpstore
+ * @copyright   2026 Yeison Díaz
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
 
 
