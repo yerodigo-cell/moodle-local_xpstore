5dcc809d5280a57b196f4c2eaf46ad10b358e9ed
yerodigo-cell <yerodigo@gmail.com>
2026-05-06 22:20:54 -0500

    Update version.php

--- version.php
+++ version.php
@@ -1,10 +1,14 @@
 <?php
 
-
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_xpstore';
 $plugin->version   = 2026050600;
-$plugin->requires  = 2022111800;
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release   = 'v2.0 (Premium Edition)';
+$plugin->requires  = 2024100700; // Versión de Moodle 4.5 (Recomendado)
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = 'v2.0';
+
+// Dependencias requeridas para que el plugin funcione
+$plugin->dependencies = array(
+    'block_xp' => ANY_VERSION // Exige que Level Up XP esté instalado
+);
