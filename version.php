8466e4fb3b8bfd56dd94b97d07b14c5b3e602c40
yerodigo-cell <yerodigo@gmail.com>
2026-05-09 23:14:52 -0500

    Update plugin version and release number

--- version.php
+++ version.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * XP Store (local_xpstore)
  *
@@ -10,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_xpstore';
-$plugin->version   = 2026050800;
+$plugin->version   = 2026050900;
 $plugin->requires  = 2024100700; // Versión de Moodle 4.5 (Recomendado)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.0.0';
+$plugin->release   = 'v1.0.1';
 
 // Dependencias requeridas para que el plugin funcione
 $plugin->dependencies = array(
