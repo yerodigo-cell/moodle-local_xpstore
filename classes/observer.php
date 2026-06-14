7651fbb7391b62b64062b2a5a0ccac2e1296db5e
yerodigo-cell <yerodigo@gmail.com>
2026-06-14 15:40:24 -0500

    Fix PSR12 opening brace space

--- classes/observer.php
+++ classes/observer.php
@@ -24,7 +24,6 @@ namespace local_xpstore;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class observer {
-
     /**
      * Intercepts course restores/clones to update hardcoded widget IDs in HTML labels and pages.
      *
