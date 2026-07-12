9691afd6e440bfee16141349abed2bbe313f634c
yerodigo-cell <yerodigo@gmail.com>
2026-07-12 18:23:13 -0500

    chore: bump version to 2.4.3

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_xpstore';
-$plugin->version   = 2026070900;
+$plugin->version   = 2026071200;
 $plugin->requires  = 2024100700;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'v2.4.3';
