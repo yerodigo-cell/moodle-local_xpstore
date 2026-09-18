80084edb73a99ecc2b2fd0b8b21f38e317b6a61b
yerodigo-cell <yerodigo@gmail.com>
2026-09-17 21:08:36 -0500

    Bump plugin version to 2026091701

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_xpstore';
-$plugin->version   = 2026091700;
+$plugin->version   = 2026091701;
 $plugin->requires  = 2024100700;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'v2.6.2';
