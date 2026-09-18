0c554334d1a2cbfbc245418c5a8dbd67bd2ba7ee
yerodigo-cell <yerodigo@gmail.com>
2026-09-18 10:59:31 -0500

    Bump version to v2.6.3

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_xpstore';
-$plugin->version   = 2026091701;
+$plugin->version   = 2026091800;
 $plugin->requires  = 2024100700;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v2.6.2';
+$plugin->release   = 'v2.6.3';
 
 // Required dependencies for the plugin to work.
 $plugin->dependencies = [
